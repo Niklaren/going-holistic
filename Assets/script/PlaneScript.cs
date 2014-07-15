4d026f004be50d7da458b64f8c98e875d2095dc5
Nick <pitfall@hotmail.co.uk>
2014-07-15 13:43:01 +0100

    moved planescript to correct folder

---
+++ Assets/script/PlaneScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneScript : MonoBehaviour {
+
+	public PlayerScript player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.transform.Translate(player.playerSpeed,0.0f,0.0f);
+	}
+}
