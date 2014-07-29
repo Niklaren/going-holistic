706335519dc3a6609e79451b550aecfe2fa3d497
pitfall <pitfall@hotmail.co.uk>
2014-07-29 05:59:51 +0100

    Score Text
    Score text is now all good baby

---
+++ Assets/script/ScoreScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreScript : MonoBehaviour {
+	
+	public PlayerScript player;
+	TextMesh textMesh;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<TextMesh>().text = player.playerScore.ToString ();
+	}
+}
